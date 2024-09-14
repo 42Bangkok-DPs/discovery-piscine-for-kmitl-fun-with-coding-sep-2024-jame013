187e0bc3bb79f378736234e5fc03748c0df5b4f5
Jame <135657301+jame013@users.noreply.github.com>
2024-09-14 17:58:49 +0700

    Update todo.js

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,63 +1,66 @@
 let list = [];
-const toDoEl = document.getElementById("ft_list");
-const addBtn = document.getElementById("addBtn");
+let toDoEl = document.getElementById("list_contain")
+let addBtn = document.getElementById("addbtn")
 
-const render = () => {
-  toDoEl.innerHTML = "";
-  list.forEach((value, index) => {
-    const toDoItem = createTodoElement(value);
-    toDoItem.addEventListener("click", () => {
-      removeTodo(index);
-    });
-    toDoEl.appendChild(toDoItem);
-  });
-};
-
-const createTodoElement = (value) => {
-  const button = document.createElement("button");
-  button.classList.add("todoItem");
+function createTodoEl(value) {
+  let button = document.createElement("button");
+  button.classList.add("text");
   button.textContent = value;
   return button;
 };
 
-const addTodo = (value) => {
+function addTodo(value) {
   list.push(value);
   updateCookie(JSON.stringify(list));
   render();
 };
 
-const removeTodo = (index) => {
+
+
+function removeTodo(index) {
   list.splice(index, 1);
   updateCookie(JSON.stringify(list));
   render();
-};
+}
 
-const updateCookie = (value) => {
+function updateCookie(value) {
   setCookie("toDo", value);
 };
 
-const setCookie = (key, value) => {
+function setCookie(key, value) {
   document.cookie = `${key} = ${value};`;
 };
 
-const getCookie = (key) => {
-  const cookies = document.cookie.split(";");
-
+function getCookie(key) {
+  let cookies = document.cookie.split(";");
+  
   for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i];
-
-    if (cookie.startsWith(key + "=")) return cookie.substring(key.length + 1);
+    let cookie = cookies[i];
+    
+    if(cookie.startsWith(key + "="))
+      return cookie.substring(key.length + 1);
     return null;
   }
 };
 
 addBtn.addEventListener("click", () => {
-  const newTodo = prompt("New ToDo");
-  if (newTodo.trim().length <= 0) return;
+  let newTodo = prompt("New ToDo");
+  if (newTodo.length <= 0) return;
   addTodo(newTodo);
 });
 
-const oldToDo = getCookie("toDo");
+function render() {
+  toDoEl.innerHTML = "";
+  list.forEach((value, index) => {
+    let todoItem = createTodoEl(value);
+    todoItem.addEventListener("click", () => {
+      removeTodo(index);
+    });
+    toDoEl.appendChild(todoItem);
+  });
+};
+
+let oldToDo = getCookie("toDo");
 if (oldToDo) {
   list = JSON.parse(oldToDo);
 }
